9983625f9dc61349fc3b03ece5df498fd17579d5
tenderfans-art <jdhilditch@gmail.com>
2026-09-13 19:26:13 +0000

    Update footer copyright

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="footer">
       <div className="shell footer-inner">
-        <strong>TenderFans</strong>
+        <strong>© TenderFans LLC 2026</strong>
         <span>All stars. No Takedowns. Just Props</span>
       </div>
     </footer>
